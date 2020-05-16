f6b6edae854c26ee6c7b4bd88197e6b36f71423e
igorskyflyer <igor.dvlpr@gmail.com>
2020-05-16 23:50:27 +0200

    Clean up

--- mp3size.js
+++ mp3size.js
@@ -6,35 +6,11 @@
  * Calculates an estimated file size of Mp3 files.
  *
  * Author: Igor Dimitrijević <igor.dvlpr@gmail.com>, 2020.
+ * Version: 1.1
  * License: MIT, see LICENSE.
  *
  * Note: does NOT validate any input, that's up to you. :)
  */
-
-/**
- * File size per second for each bitrate, see http://www.audiomountain.com/tech/audio-file-size.html.
- *
- * Format: bitrate/file size (in kbps/KB)
- * @private
- */
-const bitrateSizes = {
-  8: 1,
-  16: 2,
-  32: 4,
-  40: 5,
-  48: 6,
-  56: 7,
-  64: 8,
-  80: 10,
-  96: 12,
-  112: 14,
-  128: 16,
-  160: 20,
-  192: 24,
-  224: 28,
-  256: 32,
-  320: 40,
-}
 
 /**
  * Gets the size of 1 second of Mp3 file for the provided bitrate.
@@ -43,10 +19,10 @@ const bitrateSizes = {
  * @returns {number} the size of 1 second of Mp3 file
  */
 function getBitrate(rate = 160) {
-  if (rate in bitrateSizes) {
-    return bitrateSizes[rate]
+  if (rate >= 8) {
+    return rate / 8
   } else {
-    return bitrateSizes[160]
+    return 160 / 8
   }
 }
 
@@ -85,22 +61,20 @@ function getAudioLength(time) {
   } catch {
     return -1
   }
-
-  return -1
 }
 
 /**
  * Gets the estimated size of the Mp3 file based on the audio time and bitrate.
  * @public
  * @param {string} time the length of the Mp3 file
- * @param {number} bitrate the bitrate of the file, defaults to 160
+ * @param {number} rate the bitrate of the file, defaults to 160
  * @returns {string} the estimated Mp3 file size or 0KB in case of an error
  */
-function getFileSize(time, bitrate = 160) {
+function getFileSize(time, rate = 160) {
   const audioLength = getAudioLength(time)
 
   if (audioLength !== -1) {
-    const size = audioLength * getBitrate(bitrate)
+    const size = audioLength * getBitrate(rate)
 
     return `${size}KB`
   } else {
